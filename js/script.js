4b68e31529aaac05b3e5d7e1c09551d6f2d90cb0
Davi Pazzini <davipazzinis@gmail.com>
2024-10-11 00:17:01 -0300

    UPDATE

--- js/script.js
+++ js/script.js
@@ -16,15 +16,3 @@ faqItems.forEach((item) => {
     item.classList.toggle("active");
   });
 });
-
-// Inicialização do Swiper
-var swiper = new Swiper(".swiper-container", {
-  loop: true, // Loop infinito das imagens
-  autoplay: {
-    delay: 1500, // 1.5 segundos de intervalo entre as transições
-    disableOnInteraction: false, // Continua passando automaticamente mesmo após interação
-  },
-  slidesPerView: 1, // Mostra uma imagem por vez
-  spaceBetween: 20, // Espaçamento entre as imagens
-  effect: "slide", // Efeito de transição simples
-});
