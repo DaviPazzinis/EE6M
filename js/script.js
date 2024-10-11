1b9ec84e9be68d32bc6d7c703d5f6bc7ec2fac90
Davi Pazzini <davipazzinis@gmail.com>
2024-10-11 15:40:44 -0300

    update

--- js/script.js
+++ js/script.js
@@ -16,3 +16,33 @@ faqItems.forEach((item) => {
     item.classList.toggle("active");
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const moduleItems = document.querySelectorAll(
+    ".module-container .module-item"
+  );
+
+  moduleItems.forEach((item) => {
+    const question = item.querySelector(".module-question");
+    const answer = item.querySelector(".module-answer");
+
+    question.addEventListener("click", () => {
+      // Fechar todas as respostas ativas
+      moduleItems.forEach((i) => {
+        if (i !== item) {
+          i.classList.remove("active");
+          i.querySelector(".module-answer").style.maxHeight = "0"; // Definir altura como 0 ao fechar
+        }
+      });
+
+      item.classList.toggle("active");
+
+      if (item.classList.contains("active")) {
+        // Calcular a altura do conteúdo e aplicar a transição
+        answer.style.maxHeight = answer.scrollHeight + "px";
+      } else {
+        answer.style.maxHeight = "0"; // Voltar para altura 0 quando fechado
+      }
+    });
+  });
+});
